ed6a31d62dbb5fa4c1dd95ac980f0697ab578203
iamraufu <raufu.dev@gmail.com>
2024-09-27 16:19:39 -0400

    [Updated] Flask App

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
@@ -32,4 +33,6 @@ def predict():
     return jsonify({'prediction': list(prediction)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable, default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
